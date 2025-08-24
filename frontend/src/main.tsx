64b9af01665344bc0a2d29ab43a49c749ba1f074
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2025-08-24 16:17:33 +0200

    install rtk

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import { App } from './App';
+import { App } from './app/App';
 
-import './index.css';
+import './app/index.css';
+import { store } from './app/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
